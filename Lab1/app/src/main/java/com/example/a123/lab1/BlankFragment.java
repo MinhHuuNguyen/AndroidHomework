f88eb6abc4fb785da0cd02a45dae5fa8637416c0
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2016-09-29 18:16:31 +0700

    no message

---
+++ Lab1/app/src/main/java/com/example/a123/lab1/BlankFragment.java
@@ -0,0 +1,35 @@
+package com.example.a123.lab1;
+
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        //getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        return view;
+    }
+
+}
--- Lab1/app/src/main/java/com/example/a123/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/a123/lab1/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.a123.lab1;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAddToCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout2);
+        btnAddToCart = (Button) findViewById(R.id.btn_add_to_cart);
+        btnAddToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                BlankFragment blankFragment = new BlankFragment();
+//                blankFragment.show(fragmentManager, "fragment_edit_name");
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+                BlankFragment blankFragment = new BlankFragment();
+
+                fragmentTransaction.replace(R.id.frame_layout, blankFragment);
+
+                fragmentTransaction.addToBackStack(null);
+
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
